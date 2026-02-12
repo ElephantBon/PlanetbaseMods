4e5e81ce7d55054a1056c8ec6f5225e2831e6c9f
ElephantBon <zenbonchen@hotmail.com>
2026-02-12 18:27:33 +0800

    AssetUtility cache asset bundles

--- AssetUtility/AssetUtils.cs
+++ AssetUtility/AssetUtils.cs
@@ -1,4 +1,5 @@
 ﻿using Planetbase;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
@@ -10,14 +11,23 @@ namespace AssetUtility
     /// </summary>
     public static class AssetUtils
     {
-        public static AssetBundle LoadAssetBundle(string path)
+        private static Dictionary<string, AssetBundle> assetBundleCache = new Dictionary<string, AssetBundle>();
+
+        private static AssetBundle LoadAssetBundle(string path)
         {
-            return AssetBundle.LoadFromFile(path);
+            if(assetBundleCache.ContainsKey(path))
+                return assetBundleCache[path];
+            else {
+                var assetBundle = AssetBundle.LoadFromFile(path);
+                if(assetBundle != null)
+                    assetBundleCache[path] = assetBundle;
+                return assetBundle;
+            }
         }
 
-        public static GameObject LoadGameObject(AssetBundle assetBundle, string path)
+        public static GameObject LoadGameObject(string assetPath, string objectName)
         {
-            return assetBundle.LoadAsset<GameObject>(path);
+            return LoadAssetBundle(assetPath).LoadAsset<GameObject>(objectName);
         }
 
         public static Texture2D LoadTexture(string path)
