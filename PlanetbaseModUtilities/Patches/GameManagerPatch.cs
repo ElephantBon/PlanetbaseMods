8cf04de6824204f32ec22e6ad7d629dfd5011140
ElephantBon <zenbonchen@hotmail.com>
2026-02-12 12:03:02 +0800

    Restore ModBase in PlanetbaseModUtilities

---
+++ PlanetbaseModUtilities/Patches/GameManagerPatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PlanetbaseModUtilities
+{
+    [HarmonyPatch(typeof(GameManager), "onNewGameState")]
+    public class GameManagerPatch
+    {
+        public delegate void GameStateChangedDelegate(GameState gameState);
+        public static GameStateChangedDelegate OnGameStateChanged;
+        public static bool IsUpdating { get; private set; }
+
+        /// <summary>
+        /// Add hook at end of onNewGameState to listen for state changes
+        /// </summary>
+        [HarmonyPatch("onNewGameState")]
+        [HarmonyPostfix]
+        public static void onNewGameState(GameState gameState)
+        {
+            OnGameStateChanged?.Invoke(gameState);
+        }
+
+        [HarmonyPatch("fixedUpdate")]
+        [HarmonyPostfix]
+        public static void fixedUpdate(GameManager __instance, float timeStep)
+        {
+            IsUpdating = CoreUtils.GetMember<GameManager, GameManager.State>("mState", __instance) == GameManager.State.Updating;
+        }
+    }
+}
