8b99029670fbe4beba9615b8cf9471ba4104ab10
ElephantBon <zenbonchen@hotmail.com>
2026-02-12 13:22:42 +0800

    Add shared project AssetUtility to handle asset bundle and texture loading

---
+++ AssetUtility/AssetUtils.cs
@@ -0,0 +1,37 @@
+﻿using Planetbase;
+using System.IO;
+using UnityEngine;
+
+
+namespace AssetUtility
+{
+    /// <summary>
+    /// Require UnityEngine.AssetBundleModule.dll and UnityEngine.ImageConversionModule.dll
+    /// </summary>
+    public static class AssetUtils
+    {
+        public static AssetBundle LoadAssetBundle(string path)
+        {
+            return AssetBundle.LoadFromFile(path);
+        }
+
+        public static GameObject LoadGameObject(AssetBundle assetBundle, string path)
+        {
+            return assetBundle.LoadAsset<GameObject>(path);
+        }
+
+        public static Texture2D LoadTexture(string path)
+        {
+            var tex = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+            tex.LoadImage(File.ReadAllBytes(path));
+            tex.wrapMode = TextureWrapMode.Clamp;
+            return tex;
+        }
+
+        public static Texture2D LoadTextureColored(string path)
+        {
+            return Util.applyColor(LoadTexture(path));
+        }
+    }
+}
+
