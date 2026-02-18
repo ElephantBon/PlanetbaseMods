dd4d6112a9e498c20205f6b4e498aaab386356f2
ElephantBon <zenbonchen@hotmail.com>
2026-02-19 01:42:13 +0800

    MoreHotkeys update settings description

--- MoreHotkeys/Settings.cs
+++ MoreHotkeys/Settings.cs
@@ -8,10 +8,10 @@ namespace MoreHotkeys
         [Draw("Keybind to toggle building enable/disable. Hold Shift to toggle all buildings of same type")] public KeyCode KeyToggleBuilding = KeyCode.G;
         [Draw("Keybind to edit component of selected building")] public KeyCode KeyEditComponent = KeyCode.E;
         [Draw("Keybind to recycle all components in selected building")] public KeyCode KeyRecycleAllComponents = KeyCode.P;
-        [Draw("Keybind to complete selected construction or component instantly, or advance the progress of processor to 100%. Hold Shift to complete all or same type of the selected (Cheat)")] public KeyCode KeyInstantBuild = KeyCode.I;
-        [Draw("Keybind to damage selected construction, component, bot or human (Debug)")] public KeyCode KeyDamage = KeyCode.K;
+        [Draw("Keybind to complete selected construction or component instantly, or advance the progress of processor to 100%. Hold Shift to complete all or same type of the selected. Spawn colonist ship when selecting landing pad. (Cheat)")] public KeyCode KeyInstantBuild = KeyCode.I;
+        [Draw("Keybind to damage selected construction, component, bot or human. Remove all resources on ground if no selection. (Debug)")] public KeyCode KeyDamage = KeyCode.K;
         [Draw("Enable cheat")] public bool EnableCheat = true;
-        [Draw("Enable debug")] public bool EnableDebug = true;
+        [Draw("Enable debug")] public bool EnableDebug = false;
 
         public override void Save(UnityModManager.ModEntry modEntry)
         {
