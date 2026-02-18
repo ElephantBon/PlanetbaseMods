9f3a5751fd27b92419298a920841d13d1231d811
ElephantBon <zenbonchen@hotmail.com>
2026-02-19 03:33:50 +0800

    WhereTheDeadBodies corpse not deletable

---
+++ WhereTheDeadBodies/Patches/Resource/isDeleteablePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Planetbase;
+using WhereTheDeadBodies.Objects;
+
+namespace WhereTheDeadBodies.Patches
+{
+    [HarmonyPatch(typeof(Resource), nameof(Resource.isDeleteable))]
+    internal class isDeleteablePatch
+    {
+        public static bool Prefix(Resource __instance, out bool __result, out bool buttonEnabled)
+        {
+            __result = buttonEnabled = false;
+            if(__instance.getResourceType() is Corpse) {
+                __result = buttonEnabled = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
